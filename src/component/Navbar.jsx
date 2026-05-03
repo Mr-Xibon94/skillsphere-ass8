646da2c5eae917864830e430b459e27f24b6526b
Mr-Xibon94 <md19jibon94@gmail.com>
2026-05-03 16:48:35 +0600

    added login and logout functionality

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { authClient } from '@/lib/auth-client';
-import { Button } from '@heroui/react';
+import { Avatar, AvatarFallback, Button } from '@heroui/react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -32,30 +32,40 @@ const Navbar = () => {
                     {/* <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" /> */}
 
                     {/* this is my profile image and dropdown  */}
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img
-                                    alt="Tailwind CSS Navbar component"
-                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                    <div>
+                        {
+                            user &&
+                            <div className="dropdown dropdown-end">
+                                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                    <div className="w-10 rounded-full">
+                                        <Avatar>
+                                            <Avatar.Image
+                                            alt = "John Doe"
+                                            src={user?.image}
+                                            referrerPolicy='no-referrer'
+                                            />
+                                            <AvatarFallback>{user?.name.charAt(0)}</AvatarFallback>
+                                        </Avatar>
+                                    </div>
+                                </div>
+                                {/* user profile  */}
+                                <ul
+                                    tabIndex="-1"
+                                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                    <li>
+                                        <a className="justify-between">
+                                            Profile
+
+                                        </a>
+                                    </li>
+                                    <li><a>Settings</a></li>
+                                    <li><span onClick={handleSignOut}>LogOut</span></li>
+                                </ul>
+
                             </div>
-                        </div>
-                        {/* user profile  */}
-                        <ul
-                            tabIndex="-1"
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><span onClick={handleSignOut}>LogOut</span></li>
-                        </ul>
-
+                        }
                     </div>
-
+                    {/* login and sign out  */}
                     <div>
                         {
                             !user &&
